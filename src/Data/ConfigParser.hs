34e66e48a6b770583dd9ae5b8e8641ccdd94597c
Robin Schroer <sulami@peerwire.org>
2015-10-27 14:50:33 +0100

    add a config parser module for the new filters

---
+++ src/Data/ConfigParser.hs
@@ -0,0 +1,33 @@
+module Data.ConfigParser (
+  parseConfig
+) where
+
+import           Data.Char (toLower)
+
+data Filter = Filter
+  { want   :: Bool
+  , string :: String
+  }
+
+data Feed = Feed
+  { name    :: String
+  , filters :: [Filter]
+  }
+
+parseConfig :: String -> [Feed]
+parseConfig = map parseEntry . lines
+
+parseEntry :: String -> Feed
+parseEntry line = let whole = words $ map toLower line
+                      name = head whole
+                      filters = drop 1 whole
+                  in Feed name $ parseFilters filters
+
+parseFilters :: [String] -> [Filter]
+parseFilters []       = []
+parseFilters ("+":xs) = let (fws,rest) = break (\x -> x /= "+" && x /= "-") xs
+                        in Filter True (unwords fws) : parseFilters rest
+parseFilters ("-":xs) = let (fws,rest) = break (\x -> x /= "+" && x /= "-") xs
+                        in Filter False (unwords fws) : parseFilters rest
+parseFilters (x:xs)   = parseFilters xs
+
